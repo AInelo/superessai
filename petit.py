dd06db28a3427d6f3f2fe8540228a3114a8b2862
TOTON Lionel <totonlionel@gmail.com>
2023-12-20 03:25:41 +0100

    Le commit cool

--- petit.py
+++ petit.py
@@ -17,6 +17,7 @@ resultat = fibonacci(n)
 print(f"Les {n} premiers termes de la suite de Fibonacci sont : {resultat}")
 
 
+
 def fibonacci(n):
     if n <= 0:
         return "Entrez un entier positif pour calculer la suite de Fibonacci."
@@ -25,7 +26,7 @@ def fibonacci(n):
     elif n == 2:
         return [0, 1]
     else:
-        
+
         fib_sequence = [0, 1]
         while len(fib_sequence) < n:
             fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
