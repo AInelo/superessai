b37c0aeb9c2aaf5c5d300c26f511bf81817653ee
TOTON Lionel <totonlionel@gmail.com>
2023-12-20 03:25:04 +0100

    Le commit cool

--- petit.py
+++ petit.py
@@ -25,6 +25,7 @@ def fibonacci(n):
     elif n == 2:
         return [0, 1]
     else:
+        
         fib_sequence = [0, 1]
         while len(fib_sequence) < n:
             fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
