9fcc5fb225ea403c1d5caaba1036f00c2f194383
TOTON Lionel <totonlionel@gmail.com>
2023-12-23 03:35:14 +0100

    Le commit cool

--- palindrom.cpp
+++ palindrom.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <iostream>
+#include <cstring>
 
+bool isPalindrome(const std::string &str)
+{
+    int left = 0;
+    int right = str.length() - 1;
+
+    while (left < right)
+    {
+        if (str[left] != str[right])
+        {
+            return false;
+        }
+        ++left;
+        --right;
+    }
+
+    return true;
+}
 
 int main()
 {
