2ae396f90b9ccc76649c790dbccf22e187d587e8
TOTON Lionel <totonlionel@gmail.com>
2024-01-01 03:13:32 +0100

    Le commit cool

--- et.py
+++ et.py
@@ -12,6 +12,8 @@ def fibonacci(n):
         return fib_sequence
 
 # Exemple d'utilisation
-n = 10
+n = 12
 resultat = fibonacci(n)
 print(f"Les {n} premiers termes de la suite de Fibonacci sont : {resultat}")
+
+
